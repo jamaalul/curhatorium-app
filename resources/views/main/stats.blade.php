add0b0a6c8bc9fe206775fbcba6fefe9a6f7e38a
jamaalul <a.jamaalul@gmail.com>
2025-07-31 16:52:03 +0700

    snvknv

--- resources/views/main/stats.blade.php
+++ resources/views/main/stats.blade.php
@@ -14,8 +14,14 @@
 
 <body>
   <div class="container">
-    <div class="top-box">
+    <div class="top-box" style="position: relative;">
       <canvas id="myChart"></canvas>
+      <a href="/info/mood-tracker" class="info-button-stats" title="Info Mood Tracker" style="position: absolute; top: 12px; right: 12px; z-index: 10; color: #70c0bb !important; text-decoration: none;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#70c0bb" class="bi bi-info-circle" viewBox="0 0 16 16" style="color: #70c0bb;">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+        </svg>
+      </a>
     </div>
 
     <div class="bottom-boxes">
