e07fafc5093c929b91ab0d8a76dcfd7fb6c7e1e6
jamaalul <a.jamaalul@gmail.com>
2025-10-04 22:08:50 +0700

    ubah nama mental health test

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -140,7 +140,7 @@
         <div class="max-w-7xl mx-auto">
             <div class="text-center mb-12" data-aos="fade-up" data-aos-duration="1000">
                 <h2 class="text-3xl md:text-4xl font-bold text-[#222222]">Semua yang Anda Butuhkan</h2>
-                <p class="text-stone-600 mt-2 text-lg">Dukungan kesehatan mental yang komprehensif, dirancang khusus untuk Anda.</p>
+                <p class="text-stone-600 mt-2 text-lg">Dukungan kesejahteraan mental yang komprehensif, dirancang khusus untuk kamu.</p>
             </div>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div class="bg-white p-6 rounded-lg shadow-md text-center flex flex-col items-center gap-2" data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
@@ -175,8 +175,8 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#48a6a6" class="size-12">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
                     </svg>
-                    <h3 class="text-xl font-bold text-[#222222] mb-2">Mental Health Test</h3>
-                    <p class="text-stone-600">Tes kesehatan mental standar untuk mengenali kondisi dan kebutuhan Anda.</p>
+                    <h3 class="text-xl font-bold text-[#222222] mb-2">Tes Skala Kesejahteraan Mental</h3>
+                    <p class="text-stone-600">Tes kesejahteraan mental standar untuk mengenali kondisi dan kebutuhan Anda.</p>
                 </div>
                 <div class="bg-white p-6 rounded-lg shadow-md text-center flex flex-col items-center gap-2" data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#48a6a6" class="size-12">
@@ -268,7 +268,7 @@
                         'Calm Starter' => [
                             'price' => 0,
                             'benefits' => [
-                                '(Unlimited) Tes Kesehatan Mental',
+                                '(Unlimited) Tes Skala Kesejahteraan Mental',
                                 '(7 Hari) Mood and Productivity Tracker',
                                 '(7 Hari) Missions of The Day',
                                 '(1x) Support Group Discussion',
@@ -277,7 +277,7 @@
                         'Growth Path' => [
                             'price' => 23900,
                             'benefits' => [
-                                '(Unlimited) Tes Kesehatan Mental',
+                                '(Unlimited) Tes Skala Kesejahteraan Mental',
                                 '(Unlimited) Mood and Productivity Tracker',
                                 '(Unlimited) Missions of The Day',
                                 '(1x) Support Group Discussion',
@@ -288,7 +288,7 @@
                         'Blossom' => [
                             'price' => 59900,
                             'benefits' => [
-                                '(Unlimited) Tes Kesehatan Mental',
+                                '(Unlimited) Tes Skala Kesejahteraan Mental',
                                 '(Unlimited) Mood and Productivity Tracker',
                                 '(Unlimited) Missions of The Day',
                                 '(3x) Support Group Discussion',
@@ -299,7 +299,7 @@
                         'Inner Peace' => [
                             'price' => 86900,
                             'benefits' => [
-                                '(Unlimited) Tes Kesehatan Mental',
+                                '(Unlimited) Tes Skala Kesejahteraan Mental',
                                 '(Unlimited) Mood and Productivity Tracker w/ Extended Report',
                                 '(Unlimited) Missions of The Day',
                                 '(5x) Support Group Discussion',
