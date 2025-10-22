a35aba969771445f3c904bdad2e654984be2bcd3
jamaalul <a.jamaalul@gmail.com>
2025-10-22 21:09:15 +0700

    Reset CTA

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -18,11 +18,11 @@
             Halo {{ $user->username }} !
         </h1>
         <p class="text-base text-center text-[#222222]">Siap untuk mencari tempat nyamanmu di Curhatorium?</p>
-        <button class="bg-gradient-to-r from-[#48A6A6] to-[#357979] px-6 py-3 text-white text-lg font-semibold rounded-lg transition-all duration-300 hover:from-[#357979] hover:to-[#48A6A6] hover:scale-105 w-fit md:w-auto mt-4 shadow-lg hover:shadow-xl flex items-center justify-center gap-3 animate-pulse" onclick="window.location.href='/share-and-talk'">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+        <button class="bg-[#48A6A6] px-6 py-3 text-white rounded-md transition-colors duration-200 hover:bg-[#357979] w-fit md:w-auto mt-4 shadow-md flex items-center justify-center gap-2" onclick="window.location.href='/share-and-talk'">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
             </svg>
-            Mulai Berbagi & Berbicara Sekarang
+            Mulai Share and Talk
         </button>
     </section>
     @include('main.qotd')
