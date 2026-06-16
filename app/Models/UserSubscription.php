dfdc01fbe6485c927724bce4ebf7eb5ad3642bdb
jamaalul <a.jamaalul@gmail.com>
2026-06-16 15:14:21 +0700

    Make user_subscription table and model

---
+++ app/Models/UserSubscription.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserSubscription extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'membership_plan_id',
+        'status',
+        'current_period_start',
+        'current_period_end'
+    ];
+
+    public function membershipPlan(): BelongsTo
+    {
+        return $this->belongsTo(MembershipPlan::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
